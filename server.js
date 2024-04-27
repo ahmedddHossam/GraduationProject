dbcb49a142c30f2df8ae41aecd643a616210f2ad
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2024-04-28 01:36:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const router = require('./routes/graduateRouter')
 
 // var corsOptions = {
 //     origin: 'http://localhost:8081'
@@ -10,6 +13,12 @@ const app = express();
 // app.use(cors(corsOptions));
 // app.use(express.json);
 // app.use(express.urlencoded({ extended: true }));
+
+
+// add graduate api
+
+app.use('/api/graduates', router)
+
 
 app.get('/', (req, res) => {
     return res.send('AhmedHossam');
