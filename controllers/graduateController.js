a523e92eaa19d35d779db7f8129d46f24819274d
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2024-04-29 00:47:33 +0300

    Add files via upload

--- controllers/graduateController.js
+++ controllers/graduateController.js
@@ -5,6 +5,7 @@ const Graduate = db.graduates
 const addGraduate = async (req, res) => {
     try {
         let attributes = {
+            GraduateId: req.body.GraduateId,
             Name: req.body.Name,
             Email: req.body.Email,
             Gender: req.body.Gender,
@@ -30,6 +31,78 @@ const addGraduate = async (req, res) => {
     }
 }
 
+
+// get specific graduate
+const getOneGraduate = async (req, res) => {
+    try {
+        let id = req.params.GraduateId
+        let graduate = await Graduate.findOne({ where: { GraduateId: id }})
+        res.status(200).send(graduate)
+    } 
+    catch (error) {
+        console.error('Error getting graduate:', error);
+        res.status(500).send('Internal server error');
+    }
+
+}
+
+
+// get all graduates
+const getAllGraduates = async (req, res) => {
+    try {
+        let graduates = await Graduate.findAll()
+        res.status(200).send(graduates)
+    } 
+    catch (error) {
+        console.error('Error getting graduates:', error);
+        res.status(500).send('Internal server error');
+    }
+}
+
+
+//update graduate
+const updateGraduate = async (req, res) => {
+    try {
+        let id = req.params.GraduateId;
+        
+        const graduate = await Graduate.findOne({ where: { GraduateId: id }});
+        if (!graduate) {
+            return res.status(404).send('Graduate not found');
+        }
+        
+        await graduate.update(req.body);
+
+        res.status(200).send(graduate);
+    } catch (error) {
+        res.status(500).send('Internal server error');
+    }
+}
+
+
+
+// delete graduate 
+const deleteGraduate = async (req, res) => {
+    try {
+        let id = req.params.GraduateId;
+        
+        const graduate = await Graduate.findOne({ where: { GraduateId: id }});
+        if (!graduate) {
+            return res.status(404).send('Graduate not found');
+        }
+        
+        await graduate.destroy({ where: { GraduateId: id }});
+        res.status(200).send('Graduate deleted successfully');
+    }
+    catch (error) {
+        console.error('Error deleting graduate:', error);
+        res.status(500).send('Internal server error');
+    }
+}
+
 module.exports = {
-    addGraduate
+    addGraduate,
+    getOneGraduate,
+    getAllGraduates,
+    updateGraduate,
+    deleteGraduate
 }
