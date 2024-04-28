0d8ceeef9506eea872f76ca6decc6391f1546eaf
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2024-04-29 00:47:55 +0300

    Add files via upload

--- routes/graduateRouter.js
+++ routes/graduateRouter.js
@@ -6,5 +6,9 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/addGraduate', graduateController.addGraduate)
+router.get('/getGraduate/:GraduateId', graduateController.getOneGraduate)
+router.get('/getAllGraduates', graduateController.getAllGraduates)
+router.put('/updateGraduate/:GraduateId', graduateController.updateGraduate)
+router.delete('/deleteGraduate/:GraduateId', graduateController.deleteGraduate)
 
 module.exports = router
