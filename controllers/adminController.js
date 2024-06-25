04d7037a018f448e2ce3f216f5d93ebb38fb95f1
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2024-06-25 15:46:04 +0300

    Add files via upload

---
+++ controllers/adminController.js
@@ -0,0 +1,36 @@
+const db = require('../models');
+const Graduate = db.graduate;
+const { Sequelize, Op } = require("sequelize");
+
+// Nominate TA API
+const nominateTA = async (req, res) => {
+    try {
+        let dept = req.params.Department;
+
+        // Calculate the date 2 years ago from today
+        let twoYearsAgo = new Date();
+        twoYearsAgo.setFullYear(twoYearsAgo.getFullYear() - 2);
+        console.log(twoYearsAgo)
+
+        let graduates = await Graduate.findAll({
+            where: {
+                Department: dept,
+                EndDate: {
+                    [Sequelize.Op.gte]: twoYearsAgo
+                }
+            },
+            order: [
+                ['GPA', 'DESC']
+            ],
+            limit: 15
+        });
+
+        res.status(200).json(graduates);
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error: error.message });
+    }
+}
+
+module.exports = {
+    nominateTA
+}
