7b226d4bb92cb2af02ffecc104bf2317c82af1f7
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2024-06-25 15:46:36 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const postGraduateRouter = require('./routes/postGraduate.routes');
-const app = express();
+const multer = require('multer');
+const xlsx = require('xlsx');
+const fs = require('fs');
 
+
+const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+const upload = multer({ dest: 'uploads/' });
 const router = require('./routes/graduateRouter')
+const router2 = require('./routes/adminRouter')
 
-const path = require('path');
-const db = require("./models/index.js");
-db.sequelize.sync({ force: false })
-    .then(() => {
-        console.log('yes re-sync done!')
-    })
 // var corsOptions = {
 //     origin: 'http://localhost:8081'
 // }
@@ -25,12 +24,13 @@ db.sequelize.sync({ force: false })
 // add graduate api
 
 app.use('/api/graduates', router)
+app.use('/api/graduates', router2)
 
 
-app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
-app.use('/api/post-graduate/',postGraduateRouter);
-
+app.get('/', (req, res) => {
+    return res.send('AhmedHossam');
+});
 
 const PORT = process.env.PORT || 5000
 app.on('error', (error) => {
