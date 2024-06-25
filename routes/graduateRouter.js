3ebfb680765a66ab3576596ea7c6bd3e8fddade2
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2024-06-25 15:46:21 +0300

    Add files via upload

--- routes/graduateRouter.js
+++ routes/graduateRouter.js
@@ -1,13 +1,18 @@
 const { Model } = require('sequelize')
 const graduateController = require('../controllers/graduateController')
-
+// const adminController = require('../controllers/adminController')
+const multer = require('multer');
 const express = require('express')
 
+const upload = multer({ dest: 'uploads/' });
 const router = express.Router()
 
 router.post('/addGraduate', graduateController.addGraduate)
+router.post('/addGraduatesFromFile', upload.single('file'), graduateController.addGraduatesFromFile);
+router.get('/getAllGraduatesOfDepartment/:Department/:Year', graduateController.getAllGraduatesOfDepartment);
 router.get('/getGraduate/:GraduateId', graduateController.getOneGraduate)
 router.get('/getAllGraduates', graduateController.getAllGraduates)
+// router.get('/nominateTA/:Department', adminController.nominateTA)
 router.put('/updateGraduate/:GraduateId', graduateController.updateGraduate)
 router.delete('/deleteGraduate/:GraduateId', graduateController.deleteGraduate)
 
